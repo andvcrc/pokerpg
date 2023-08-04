2f07b7069b413bb80ff9bfe5cdfd1a7dd04356d3
andvcrc <apolofostry@gmail.com>
2023-08-04 12:09:58 -0300

    Design changes

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -6,26 +6,26 @@ const Pokemon = ({ pokemon }) => {
       <h4>{pokemon.nome}</h4>
       <div className="row" style={{ justifyContent: "center" }}>
       <>Nature: {pokemon.nature}</>
-      <>Tipo: {pokemon.tipo}</>
-      <>Nível: {pokemon.nivel}</>
+      <> | Tipo: {pokemon.tipo}</>
+      <> | Nível: {pokemon.nivel}</>
       </div>
       <> {pokemon.pontos_de_vida}</>
       <div className="row" style={{ justifyContent: "center" }}>
         <h4>Atributos</h4>
         <>Saúde: {pokemon.status.saude}</>
-        <> Ataque: {pokemon.status.ataque}</>
-        <> Defesa: {pokemon.status.defesa}</>
+        <> | Ataque: {pokemon.status.ataque}</>
+        <> | Defesa: {pokemon.status.defesa}</>
       </div>
       <div className="row" style={{ justifyContent: "center" }}>
         <>Ataque Especial: {pokemon.status.atq_esp}</>
-        <> Defesa Especial: {pokemon.status.def_esp}</>
-        <> Velocidade: {pokemon.status.velocidade}</>
+        <> | Defesa Especial: {pokemon.status.def_esp}</>
+        <> | Velocidade: {pokemon.status.velocidade}</>
       </div>
       <div className="row" style={{ justifyContent: "center" }}>
         <h4>Evasões</h4>
         <>Física: {pokemon.evasao.fisica}</>
-        <> Especial: {pokemon.evasao.especial}</>
-        <> Veloz: {pokemon.evasao.veloz}</>
+        <> | Especial: {pokemon.evasao.especial}</>
+        <> | Veloz: {pokemon.evasao.veloz}</>
       </div>
       <> {pokemon.bonus_elemental}</>
       <p>
